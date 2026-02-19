09b23a42e8e79038a4d7e90e8f099d7aa54bea2f
dzony9pozdro <jan@tomaka.it>
2026-02-20 00:03:50 +0100

    fixed edge cases

--- numberPickingGame.py
+++ numberPickingGame.py
@@ -1,28 +1,29 @@
 import random
-def generate():
-    global userDefinedRange
-    userDefinedRange =int(input('how many numbers do you want to pick out of?: '))
-    global winningNumber
-    winningNumber = round(userDefinedRange*random.random())
-    # print(winningNumber)
-def guess():
-    global  userGuess
-    userGuess = int(input(f'input your guess in the range of 0 and {int(userDefinedRange)}: '))
-def check():
-    global playingAgain
+playing_again = "y"
+while playing_again == "y":
 
-    if userGuess == winningNumber:
-        playingAgain =  input('congrats, wanna play again? (y/n): ')
-    else: playingAgain = input('loser! wanna play again? (y/n): ')
-    playingAgain.lower
-    match playingAgain:
-        case "y":
-            gameLoop()
-        case "n":
-            print('gg ez bye')
+    tries = 0
+    # prompt the user for a range of integers 0 - userDefinedRange.
+    user_defined_range = int(input('input the upper limit of the range you want to play in: '))
 
-def gameLoop():
-    generate()
-    guess()
-    check()
-gameLoop()
+    # generate a random number within that range of integers.
+    winning_number = random.randint(0, user_defined_range)
+    # prompt the user for their guess
+    while True:
+
+        try: 
+            user_guess = int(input(f'input a number in the range of 0 to {user_defined_range} inclusive: '))
+            break
+        except ValueError:
+            user_guess = print(f'please input a number 0 through {user_defined_range} inclusive: ')
+    # check if the userGuess is winning, below the target, above the target, or outside of the definedd range
+    while user_guess != winning_number:
+        tries+=1
+        if not (0 <= user_guess <= user_defined_range):
+            user_guess = int(input(f'inputted number was outside of the defined range: 0 through {user_defined_range}, try again: '))
+        elif user_guess > winning_number:
+            user_guess = int(input(f'tries: {tries}  {user_guess} is too high! try again:'))
+        elif user_guess < winning_number:
+            user_guess = int(input(f'tries: {tries}  {user_guess} is too low! try again:'))
+    # determine if the player wants to play again, run the gameLoop again, or exit the game.
+    playing_again = input(f'congrats {user_guess} is the correct number, you got it in {tries} tries wanna play again (y/n: )').lower()
